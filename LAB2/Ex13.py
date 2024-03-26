22d0892ae4f76bc19ac5376fbbd2a48adeb60efb
Sergiu <sergiusay06@gmail.com>
2024-03-26 00:05:29 -0700

    aaaa

---
+++ LAB2/Ex13.py
@@ -0,0 +1,24 @@
+#Să se realizeze un program care citește două numere reale de la tastatură și apoi
+#afișează un meniu cu următoarele opțiuni:
+#a. Suma numerelor introduse
+#b. Diferența numerelor introduse
+#c. Produsul numerelor introduse
+#d. Raportul numerelor introduse
+#e. Media aritmetică a numerelor
+
+
+a = float(input("introduceti a: "))
+b = float(input("introduceti b: "))
+
+while(1):
+
+    print("a. Suma numerelor introduse")
+    print("b. Diferența numerelor introduse")
+    print("c. Produsul numerelor introduse")
+    print("d. Raportul numerelor introduse")
+    print("e. Media aritmetică a numerelor")
+
+    n = chr(input('introdu optiunea: '))
+
+
+
