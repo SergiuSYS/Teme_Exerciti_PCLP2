9180b2c1052fc810597643a60e318d2447f39752
Sergiu <sergiusay06@gmail.com>
2024-05-10 16:18:53 +0300

    .

--- LAB8(Grafice in Python)/Ex10.py
+++ LAB8(Grafice in Python)/Ex10.py
@@ -6,12 +6,12 @@ x = np.linspace(0, 3*np.pi, 360)
 sinus = np.sin(x)
 cosinus = np.cos(x)
 tangenta = np.tan(x)
-cotangenta =1/np.tan(x)
+cotangenta =1/np.tan(x)#nu merge nush de ce 
 
 plt.plot(x, sinus)
 plt.plot(x, cosinus)
-#plt.plot(x, tangenta)
-#plt.plot(x, cotangenta)
+# plt.plot(x, tangenta)
+# plt.plot(x, cotangenta)
 
 plt.grid(True)
 plt.show()
